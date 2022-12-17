fa2a66987b5850cc83aae43fdd7dd5f85530b741
xaskilaki007 <115955213+xaskilaki007@users.noreply.github.com>
2022-12-17 18:14:36 +0300

    Create zadanie 3.cpp

---
+++ zadanie 3.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cmath>
+
+intmain(){
+float x,y;
+std::cout«"Enter_x"«std::endl;
+std::cin»x;
+std::cout«"Enter_y"«std::endl;
+std::cin»y;
+if((y==-x) || (y==x)){
+std::cout«"v zone"«std::endl;
+}else std::cout«"ne v zone"«std::endl;
+}
